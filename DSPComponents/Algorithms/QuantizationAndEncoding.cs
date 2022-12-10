cb882b0c3fc1d13a390118fd3061dd3ddf32c842
Menna-t-Allah Wael <MennatAllahWael246@gmail.com>
2022-12-10 18:55:40 +0200

    Task 2

--- DSPComponents/Algorithms/QuantizationAndEncoding.cs
+++ DSPComponents/Algorithms/QuantizationAndEncoding.cs
@@ -18,10 +18,60 @@ namespace DSPAlgorithms.Algorithms
         public List<int> OutputIntervalIndices { get; set; }
         public List<string> OutputEncodedSignal { get; set; }
         public List<float> OutputSamplesError { get; set; }
-
+        /*public int to_binary(int n)
+        {
+            int x = 0;
+            while(n != 0)
+            {
+                x *= 10;
+                x += n % 10;
+                n /= 10;
+            }
+            return x;
+        }*/
         public override void Run()
         {
-            throw new NotImplementedException();
+            List<float> midpoints = new List<float>();
+            List<float> intervals = new List<float>();
+            List<float> samples = new List<float>();
+
+            OutputSamplesError = new List<float>();
+            OutputEncodedSignal = new List<string>();
+            OutputIntervalIndices = new List<int>();
+
+            if (InputNumBits != 0)
+                InputLevel = (int)Math.Pow(2, InputNumBits);
+            else if (InputLevel != 0)
+                InputNumBits = (int)Math.Log(InputLevel, 2);
+
+            float maxi, mini, delta;
+            maxi = InputSignal.Samples.Max();
+            mini = InputSignal.Samples.Min();
+            delta = (maxi - mini) / InputLevel;
+
+            for (int i = 0; i <= InputLevel; i++)
+            {
+                intervals.Add(mini);
+                if (i < InputLevel)
+                    midpoints.Add((mini * 2 + delta) / 2);
+                mini += delta;
+            }
+            // midpoints & intervals are calculated
+            for (int i = 0; i < InputSignal.Samples.Count; i++)
+            {
+                for (int j = 0; j < InputLevel; j++)
+                {
+                    if (InputSignal.Samples[i] >= intervals[j] && InputSignal.Samples[i] <= intervals[j + 1] + 0.0001)
+                    {
+                        OutputIntervalIndices.Add(j + 1);
+                        OutputSamplesError.Add(midpoints[j] - InputSignal.Samples[i]);
+                        OutputEncodedSignal.Add(Convert.ToString(j, 2).PadLeft(InputNumBits, '0'));
+                        samples.Add(midpoints[j]);
+                        break;
+                    }
+                }
+            }
+            OutputQuantizedSignal = new Signal(samples, false);
         }
     }
 }
---
+++ DSPComponents/Algorithms/SinCos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSPAlgorithms.DataStructures;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class SinCos : Algorithm
+    {
+        public string type { get; set; }
+        public float A { get; set; }
+        public float PhaseShift { get; set; }
+        public float AnalogFrequency { get; set; }
+        public float SamplingFrequency { get; set; }
+        public List<float> samples { get; set; }
+        public override void Run()
+        {
+            samples = new List<float>();
+            float fs = (float)(AnalogFrequency / SamplingFrequency);
+            if (type == "sin")
+                for (int i = 0; i < SamplingFrequency; i++)
+                    samples.Add((float)(1.0 * A * (Math.Sin(2 * Math.PI * fs * i + PhaseShift))));
+            else if (type == "cos")
+                for (int i = 0; i < SamplingFrequency; i++)
+                    samples.Add((float)(1.0 * A * (Math.Cos(2 * Math.PI * fs * i + PhaseShift))));
+        }
+    }
+}
