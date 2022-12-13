a0af996abba7ccda565a3a8c2bf1c921c86e11fe
Menna-t-Allah Wael <MennatAllahWael246@gmail.com>
2022-12-14 00:29:46 +0200

    Task 3

--- DSPComponents/Algorithms/DiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/DiscreteFourierTransform.cs
@@ -15,7 +15,37 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            int N = InputTimeDomainSignal.Samples.Count;
+            List<float> phase, amplitude;
+            //real = new List<float>();
+            //img = new List<float>();
+            amplitude = new List<float>();
+            phase = new List<float>();
+            //freq = new List<float>();
+            float x, y;
+
+
+            for (int k = 0; k < N; k++)
+            {
+                x = 0;
+                y = 0;
+                for (int n = 0; n < N; n++)
+                {
+                    x += InputTimeDomainSignal.Samples[n] * ((float)Math.Cos((float)2 * Math.PI * k * n / N));
+                    y += InputTimeDomainSignal.Samples[n] * (-1.0f * (float)Math.Sin((float)2 * Math.PI * k * n / N));
+                }
+                //real.Add(x);
+                //img.Add(y);
+                amplitude.Add((float)Math.Sqrt(x * x + y * y));
+                phase.Add((float)Math.Atan2(y, x));
+            }
+            //float f = (float)(2 * Math.PI * InputSamplingFrequency / N);
+            //for(int i = 0; i < N; i ++)
+            //    freq.Add(f * i);
+            OutputFreqDomainSignal = new Signal(InputTimeDomainSignal.Samples, true);
+            OutputFreqDomainSignal.FrequenciesAmplitudes = new List<float>(amplitude);
+            OutputFreqDomainSignal.FrequenciesPhaseShifts = new List<float>(phase);
+            //OutputFreqDomainSignal.Frequencies = new List<float>(freq);
         }
     }
 }
--- DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSPAlgorithms.DataStructures;
-
+using System.Numerics;
 namespace DSPAlgorithms.Algorithms
 {
     public class InverseDiscreteFourierTransform : Algorithm
@@ -14,7 +14,34 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            int N = 0;
+            //if(InputFreqDomainSignal.FrequenciesAmplitudes != null)
+            N = InputFreqDomainSignal.FrequenciesAmplitudes.Count;
+            Complex c, e, sum;
+            List<Complex> x = new List<Complex>();
+            List<float> output = new List<float>();
+            double real, img;
+            for (int i = 0; i < N; i++)
+            {
+                real = (double)InputFreqDomainSignal.FrequenciesAmplitudes[i] * Math.Cos(InputFreqDomainSignal.FrequenciesPhaseShifts[i]);
+                img = (double)InputFreqDomainSignal.FrequenciesAmplitudes[i] * Math.Sin(InputFreqDomainSignal.FrequenciesPhaseShifts[i]);
+                c = new Complex(real, img);
+                x.Add(c);
+            }
+            for (int k = 0; k < N; k++)
+            {
+                sum = new Complex();
+                for (int n = 0; n < N; n++)
+                {
+                    real = Math.Cos(2 * Math.PI * k * n / N);
+                    img = Math.Sin(2 * Math.PI * k * n / N);
+                    e = new Complex(real, img);
+                    sum += x[n] * e;
+                }
+                sum /= N;
+                output.Add((float)sum.Real);
+            }
+            OutputTimeDomainSignal = new Signal(output, false);
         }
     }
 }
