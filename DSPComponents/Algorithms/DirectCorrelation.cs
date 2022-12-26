55f90b2fb0fe5e7f2f7b29d5ae5ae3535ff3ba4c
Menna-t-Allah Wael <MennatAllahWael246@gmail.com>
2022-12-26 16:31:59 +0200

    Task 4

--- DSPComponents/Algorithms/DirectCorrelation.cs
+++ DSPComponents/Algorithms/DirectCorrelation.cs
@@ -16,7 +16,44 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            OutputNonNormalizedCorrelation = new List<float>();
+            OutputNormalizedCorrelation = new List<float>();
+
+            float Norm1 = 0, Norm2 = 0, Norm = 0;
+            int N = InputSignal1.Samples.Count, N2;
+
+            if (InputSignal2 == null)
+                InputSignal2 = new Signal(InputSignal1.Samples, false);
+            N2 = InputSignal2.Samples.Count;
+            N = Math.Max(N, N2);
+
+            for (int i = 0; i < N; i++)
+            {
+                Norm1 += (float)Math.Pow(InputSignal1.Samples[i], 2);
+                Norm2 += (float)Math.Pow(InputSignal2.Samples[i], 2);
+                InputSignal2.Samples.Add(0);
+                InputSignal1.Samples.Add(0);
+            }
+            Norm = (float)Math.Sqrt(Norm1 * Norm2) / N;
+
+            int k;
+            float Ans;
+
+            for (int i = 0; i < N; i++)
+            {
+                Ans = 0;
+                for (int j = 0; j < N; j++)
+                {
+                    if (InputSignal1.Periodic)
+                        k = (i + j) % N;
+                    else
+                        k = i + j;
+                    Ans += InputSignal1.Samples[j] * InputSignal2.Samples[k];
+                }
+                Ans /= N;
+                OutputNonNormalizedCorrelation.Add(Ans);
+                OutputNormalizedCorrelation.Add(Ans / Norm);
+            }
         }
     }
 }
